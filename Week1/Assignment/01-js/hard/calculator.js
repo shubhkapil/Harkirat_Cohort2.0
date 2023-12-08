c4b538b2f90dbd4ae1f5d8aea3334aaa95e84f45
shubhkapil <shubhkapil2000@gmail.com>
2023-12-09 01:53:36 +0530

    Update calculator.js

--- Week1/Assignment/01-js/hard/calculator.js
+++ Week1/Assignment/01-js/hard/calculator.js
@@ -16,6 +16,236 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+
+
+
+
+//Stack Implemented Using List
+class Stack{
+
+  constructor()
+  {
+    this.items = []
+  }
+
+  push(data) {
+      this.items.push(data);  
+  }
+  size()
+  {
+    return this.items.length;
+  }
+  isEmpty()
+  {
+    return (this.items.length == 0);
+  }
+  pop()
+  {
+    if(this.isEmpty())
+      return -Infinity;
+    this.items.pop();
+  }
+  top()
+  {
+    return this.items[this.size() - 1];
+  }
+}
+
+
+//Function to check precedency of different Operators
+function precedency(c)
+{
+  if(c=='^')  return 4;
+  else if(c=='*' || c=='/')  return 3;
+  else if(c=='+' || c=='-')  return 2;
+  else return -1;
+}
+
+//Function to find result(for string expression)
+function calulateResult(a,b,s)
+{
+  c = s[0]
+  switch(c)
+  {
+    case '^': return Math.pow(a,b);
+    case '*': return a*b;
+    case '/': return a/b;
+    case '+': return a+b;
+    case '-': return a-b;
+    default : return -Infinity;
+  }
+}
+
+function isOperator(s)
+{
+  if(s=="^" || s=="*" || s=="/" || s=="+" || s=="-")
+    return true;
+  return false;
+}
+
+function printStack(s)
+{
+  if(s.isEmpty())
+    return
+  let a = s.top();
+  s.pop();
+  printStack(s)
+  console.log(a);
+  s.push(a);
+}
+
+//Coversion Infix->Postfix (for expression evaluation)
+function infixToPostfix(str)
+{
+  var v = [];
+  var st = new Stack();
+
+  for(let i = 0;i<str.length;i++)
+  {
+    var c = str[i];
+    if(c==' ')
+      continue;
+
+    if(c>='0' && c<='9')
+    {
+      temp = "";
+      decimalSeen = false;
+      while((i<str.length && str[i]>='0' && str[i]<='9') || (str[i]=='.' && decimalSeen==false))
+      {
+        if(str[i]=='.' && decimalSeen==true)
+        {
+          throw new Error("Error");
+        }
+        else if(str[i]=='.')
+          decimalSeen = true;
+        temp+=str[i++]
+      }
+      v.push(temp);
+      --i;
+    }
+
+    else if(c=='(')
+      st.push(c);
+    
+
+    else if(c==')')
+    {
+      if(st.isEmpty())
+        throw new Error("Error");
+
+      while(st.top()!='(')
+      {
+        v.push(st.top()+"")
+        st.pop();
+
+        if(st.isEmpty())
+        throw new Error("Error");
+      }
+        
+      st.pop();
+    }
+
+    else
+    {
+      if(precedency(c)==-1)
+      {
+        throw new Error("Error");
+      }
+
+      else
+      {
+        while(!st.isEmpty() && precedency(c) <= precedency(st.top()))
+        {
+          v.push(st.top()+"");
+          st.pop();
+        }
+        st.push(c)
+      }
+    }
+  }
+  while(!st.isEmpty())
+  {
+    v.push(st.top());
+    st.pop();
+  }
+  return v;
+}
+
+class Calculator {
+
+  #result = 0;
+  #st = new Stack();
+  
+  add(data)
+  {
+    this.#result+=data;
+  }
+  subtract(data)
+  {
+    this.#result-=data;
+  }
+  multiply(data)
+  {
+    this.#result*=data;
+  }
+  divide(data)
+  {
+    if(data==0)
+      throw new Error("Error");
+    this.#result/=data;
+    
+  }
+  clear()
+  {
+    this.#result = 0;
+  }
+
+  getResult()
+  {
+    return this.#result;
+  }
+
+  calculate(str)
+  {
+    let v = infixToPostfix(str)
+    
+    console.log(v)
+    for(let i =0;i<v.length;i++)
+    {
+      if(v[i]=='(' || v[i]==')')
+        throw new Error('Error');
+
+      if(isOperator(v[i]))
+      {
+        if(this.#st.size()<2)
+        {
+          throw new Error("Error");
+        }
+        else
+        {
+          let a = Number(this.#st.top());
+          this.#st.pop();
+          let b = Number(this.#st.top());
+          this.#st.pop();
+          if(v[i]=='/' && a==0)
+          {
+            throw new Error("Error");
+          }
+
+          this.#st.push(calulateResult(b,a,v[i]))
+        }
+      }
+      else 
+        this.#st.push(Number(v[i]));
+    }
+    
+    if(this.#st.isEmpty())
+      throw new Error("Error");
+    this.#result =  this.#st.top();
+  }
+}
+
+
 
 module.exports = Calculator;
+
